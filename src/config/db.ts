0b1d011cea36430239f650650e1acee273378813
lucascostamagna19@outlook.com <lucascostamagna19@outlook.com>
2024-12-17 19:42:00 -0300

    fix

--- src/config/db.ts
+++ src/config/db.ts
@@ -5,8 +5,10 @@ dotenv.config()
 
 // ! le dice que si o si va a existir la variable de entorno
 const db = new Sequelize(process.env.DATABASE_URL!, {
-    models: [__dirname + '/../models/**/*'],
-    logging: false
+    models: [__dirname + "/../models/**/*"],
+    logging: false,
+    dialect: 'postgres',
+    dialectModule: require('pg')
 })
 
 export default db
